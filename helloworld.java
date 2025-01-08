8d9cdb099f4b3e9116aa289e6a74ceb667f3e066
anishlowanshi <anishlowanshi090@gmail.com>
2025-01-08 19:19:24 +0530

    Multiply added

--- helloworld.java
+++ helloworld.java
@@ -6,7 +6,13 @@ Class Main{
     {
         return (a-b);
     }
-    public static void main(String[] args{
-        System.out.println("Hello anish");
+
+    public static int multiply(int a, int b) {
+        return ( a*b); 
     }
+    public static void main(String[] args{
+        System.out.println("Hello anish"); 
+
+    } 
+
 }
