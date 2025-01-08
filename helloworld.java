5be6829fc5c80f50c556f5f9c4b2208c32d6a933
anishlowanshi <anishlowanshi090@gmail.com>
2025-01-08 19:35:19 +0530

    division added

--- helloworld.java
+++ helloworld.java
@@ -9,9 +9,16 @@ Class Main{
 
     public static int multiply(int a, int b) {
         return ( a*b); 
-    }
+    } 
+    public static int divide(int a, int b) {
+        return ( a/b); 
+    } 
+     
+     
+
     public static void main(String[] args{
         System.out.println("Hello anish"); 
+ 
 
     } 
 
